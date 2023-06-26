72acb1e71aad59d4dcac35e4e23155050c2db70a
Jindrich Bartek <jindrich@baserank.io>
2023-06-26 17:43:49 +0200

    feat(FE): #1: Revamp overall UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,26 +40,43 @@ const ChatGPTClient: React.FC = () => {
     
   };
 
+  const messageType = (message: any) => {
+    return message.type === 'user' ? 'user-message' : 'chatgpt-message'
+  }
+
   return (
-    <div>
-      <h1>ChatGPT Client</h1>
-      <div>
-        {conversation.map((message, index) => (
-          <div key={index} className={message.type === 'user' ? 'user-message' : 'chatgpt-message'}>
-            {message.text}
-          </div>
-        ))}
-      </div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={userInput}
-          onChange={(e) => setUserInput(e.target.value)}
-          placeholder="Type your message"/>
-          <button type="submit">Send</button>
-        </form>
-      </div>
-    );
-  };
+    <>
+      <header>
+        <h1 className='px-4'>Chat GPT</h1>
+      </header>
+          <main className='relative flex flex-1 min-h-screen h-full w-full'>
+            <section id='section1' className='flex flex-col w-full items-center justify-start
+              max-h-85vh flex-basis-20 overflow-auto'>
+              { conversation.map((message, index) => (
+                <article key={ index } className={ `text-left md:w-1/2 w-full ${messageType(message)}` }>
+                  { message.text }
+                </article>
+              )) }
+            </section>
+            <section id='section2' className='absolute bottom-4vh left-0 w-full flex justify-center'>
+              <form className='flex items-center justify-center w-full' onSubmit={ handleSubmit }>
+                <textarea
+                  className='border rounded-md shadow-md md:w-3/4 w-full p-2 ml-4'
+                  value={ userInput }
+                  onChange={(e) => setUserInput(e.target.value)}
+                  placeholder="Type your prompt."
+                />
+                <button
+                  type="submit"
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold mx-4 py-2 px-4 rounded"
+                >
+                  Send
+                </button>
+              </form>
+            </section>
+          </main>
+    </>
+  );
+};
 
   export default ChatGPTClient;
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,12 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      maxHeight: {
+        '85vh': '85vh',
+      },
+      spacing: {
+        '4vh': '4vh',
+      }
     },
   },
   plugins: [],
